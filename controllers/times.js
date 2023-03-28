56b24c93ac5a1c325a82b0d45cce60cb81a95ef3
joaopageno2 <joao_pageno@estudante.sc.senai.br>
2023-03-28 20:59:33 -0300

    INSERT quartos

--- controllers/times.js
+++ controllers/times.js
@@ -1,40 +1,14 @@
 const database = require('./../data/database')
 
-
-exports.insertPartida = (req, res, next) => {    
-    const novaPartida = req.body
-    const query = 'INSERT INTO Placares(id_partida, data, time_1, placar_1, placar_2, time_2) VALUES ($1, $2, $3, $4, $5, $6)'
-    const values = [novaPartida.id_partida, novaPartida.data, novaPartida.time_1, novaPartida.placar_1, novaPartida.placar_2, novaPartida.time_2]
-
-    if(!novaPartida?.time_1 || novaPartida?.time_2 == undefined) {
-        res.status(404).send('Times não localizados');
-    } else if (novaPartida.placar_1 == novaPartida.placar_2) {
-        database.query = `UPDATE pontuacao SET empates = empates + 1 WHERE time=${novaPartida.time_1}`
-        database.query = `UPDATE pontuacao SET empates = empates + 1 WHERE time=${novaPartida.time_2}`
-        database.query = `UPDATE pontuacao SET pontuacao = pontuacao + 1 WHERE time=${novaPartida.time_1}`
-        database.query = `UPDATE pontuacao SET pontuacao = pontuacao + 1 WHERE time=${novaPartida.time_2}`
-        res.status(200).send(pontuacao)
-
-    } else if (novaPartida.placar_1 > novaPartida.placar_2) {
-        database.query = `UPDATE pontuacao SET vitoria = vitoria + 1 WHERE time=${novaPartida.time_1}`
-        database.query = `UPDATE pontuacao SET derrotas = derrotas + 1 WHERE time=${novaPartida.time_2}`
-        database.query = `UPDATE pontuacao SET pontuacao = pontuacao + 3 WHERE time=${novaPartida.time_1}`
-        res.status(200).send(pontuacao)
-
-    } else {
-        database.query = `UPDATE pontuacao SET vitoria = vitoria + 1 WHERE time=${novaPartida.time_2}`
-        database.query = `UPDATE pontuacao SET derrotas = derrotas + 1 WHERE time=${novaPartida.time_1}`
-        database.query = `UPDATE pontuacao SET pontuacao = pontuacao + 3 WHERE time=${novaPartida.time_2}`
-        res.status(200).send(pontuacao)
-    }
-    
+exports.inserirTimeEquipePontuacao = (req, res, next) => {    
+    const novoQuarto = req.body
+    const query = 'INSERT INTO Quartos(Num, num_vagas, disponivel, total_hospedes) VALUES ($1, $2, $3, $4)'
+    const values = [novoQuarto.Num, novoQuarto.num_vagas, novoQuarto.disponivel, novoQuarto.total_hospedes]
 
     database.query(query, values).then(() => {
-        res.status(200).send({ mensagem: 'nova partida' })
-        database.query = 'SELECT * FROM Placares'
+        res.status(200).send({ mensagem: 'Quarto cadastrado' })
+        database.query = 'SELECT * FROM Quartos'
     }, (erro) => {
         res.status(500).send({ erro: erro })
     })
 }
-
-
